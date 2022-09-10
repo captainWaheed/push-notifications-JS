a25a15fc08dde75fc0d1b34243f2a3dfc8823cea
captainWaheed <thecaptain0078@gmail.com>
2022-09-10 22:21:05 +0500

    notification changes

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   Notification.requestPermission().then((prem) => {
-    alert(perm);
+    if (prem === "granted") {
+      const notification = new Notification("Example notification", {
+        body: "This is description text",
+        data: { hello: "world" },
+        icon: "https://cdn-icons-png.flaticon.com/512/2058/2058148.png",
+        tag: "Welcome message",
+      });
+      notification.addEventListener("error", (e) => {
+        alert("error");
+      });
+    }
   });
+});
+
+let notification;
+let interval;
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    setInterval(() => {
+      notification = new Notification("Come back please", {
+        body: `You have been gone for ${Math.round(
+          (new Date() - leaveDate) / 1000
+        )} seconds`,
+        tag: "Come back",
+      });
+    }, 100);
+  } else {
+    if (interval) clearInterval(interval);
+    if (notification) notification.close();
+  }
 });
